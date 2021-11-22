7dd8acba9bbf93f4c17b977d9a8c31ef584f3771
KDW999 <hide4321@naver.com>
2021-11-22 22:39:22 +0900

    테스트

--- ui/pages/rooms/index.tsx
+++ ui/pages/rooms/index.tsx
@@ -1,8 +1,39 @@
+import { title } from "process";
 import React from "react";
 import * as ws_manager from "../../utils/ws_manager";
 
-const Rooms: React.FC = ({}) => {
-  return <div></div>;
-};
+function Rooms(){
+ // let post = '1번 방'; // 데이터 바인딩
+
+  return(
+    <div>
+      
+        <div style={{color : 'green', fontSize:'40px'}}>Title</div>
+
+      
+      <h3 style={{width: '300px', height: '45px', margin : '50px', backgroundColor: 'silver'}}> 1번 방 </h3>
+      
+      
+      <div className='list'>
+      <h3>2번 방</h3>
+      </div>
+
+      <div className='list'>
+      <h3>3번 방</h3>
+      </div>
+    </div>
+  );
+}
+// const Rooms: React.FC = ({}) => {
+  
+//   let post = "테스팅";
+//   return (
+//     <div>연습용 만들기
+//       <h3>{post}</h3>
+//       </div>
+//   );
+//   // return <div>뭐지??</div>;
+// };
+
 
 export default Rooms;
