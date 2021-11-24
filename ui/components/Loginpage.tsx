16bea1b2d6bc2e72fafd892d690e18d31d8c8c4f
YJH57 <90899728+YJH57@users.noreply.github.com>
2021-11-24 19:47:05 +0900

    login
    login

---
+++ ui/components/Loginpage.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+function Loginpage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {setEmail(event.currentTarget.value)}
+    const onPasswordHandler = (event) => {setPassword(event.currentTarget.value)}
+    const onSubmitHandler=(event)=>{event.preventDefault()
+        let body={email: Email, password: Password}
+    } 
+
+    const Loginpage = () => {
+        return (
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', width: '100%', height:'100vh'}}>
+            <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={onSubmitHandler}>
+            <label>ID</label>
+            <input type="email" value={Email} onChange={onEmailHandler} />
+            <button>Login</button>
+
+            <br />
+
+            <label>PW</label>
+            <input type="password" value={Password} onChange={onPasswordHandler} />
+            </form>
+        </div>
+        );
+    }
+};
+
+export default Loginpage;
